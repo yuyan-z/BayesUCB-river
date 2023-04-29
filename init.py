79e48815feb4018c35e081bc54b122d8c03b51fe
jaulmes <bjaulmes@yahoo.fr>
2023-04-29 18:13:08 +0200

    added graph

--- init.py
+++ init.py
@@ -13,13 +13,18 @@ if __name__ == '__main__':
     policy = BayesUCB(n_arms=env.action_space.n)
 
     metric = stats.Sum()
+    hist_metrics = []
     while True:
         action = next(policy.pull(range(env.action_space.n)))
         observation, reward, terminated, truncated, info = env.step(action)
         policy = policy.update(action, reward)
         policy.getReward(action, reward)
         metric = metric.update(reward)
+        hist_metrics.append(metric.get())
         if terminated or truncated:
             break
 
     print('Sum:', metric.get())
+    plt.figure()
+    plt.plot(hist_metrics)
+    plt.show()
