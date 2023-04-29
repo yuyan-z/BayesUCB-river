ab473d1d19efe487596a3510692a2485e82ab038
jaulmes <bjaulmes@yahoo.fr>
2023-04-29 17:07:21 +0200

    added comments

--- BayesUCB.py
+++ BayesUCB.py
@@ -6,6 +6,15 @@ import random
 from Beta import Beta
 
 class BayesUCB(bandit.base.Policy):
+    '''
+    This class represents a Bayes UCB model. It is a child of the bandit.base.Policy class in River.
+    Parameters :
+    n_arms : int. Number of arms.
+    t : int. Time.
+    posterior : dict. Contains the posterior distributions of all the arms.
+    reward_obj : The reward object used to measure the performance of each arm. This can be a metric, a statistic, or a distribution.
+    burn_in : The number of steps to use for the burn-in phase. Each arm is given the chance to be pulled during the burn-in phase. This is useful to mitigate selection bias.
+    '''
     def __init__(self, n_arms: int, reward_obj=None, burn_in=0):
         super().__init__(reward_obj, burn_in)
 
@@ -18,6 +27,11 @@ class BayesUCB(bandit.base.Policy):
             self.posterior[arm_id].reset()
 
     def _pull(self, arm_ids):
+        '''
+        Pulls an arm of the bandit. It randomly chooses an arm among those with the highest expected reward.
+        Parameters :
+        arm_ids : list. List of arms in which to search for the best arm.
+        '''
         index = dict()
         for arm_id in arm_ids:
             index[arm_id] = self.computeIndex(arm_id)
@@ -27,8 +41,19 @@ class BayesUCB(bandit.base.Policy):
         return random.choice(bestArms)
 
     def computeIndex(self, arm_id):
+        '''
+        Returns the quantile of the posterior distribtion of a given arm.
+        Parameters :
+        arm_id : index of the arm.
+        '''
         return self.posterior[arm_id].quantile(1 - 1. / self.t)
 
-    def getReward(self, arm, reward):
-        self.posterior[arm].update(reward)
+    def getReward(self, arm_id, reward):
+        '''
+        Update the posterior distribution based on the reward associated to this arm.
+        Parameters :
+        arm_id : index of the arm
+        reward : reward that was obtained from the environment.
+        '''
+        self.posterior[arm_id].update(reward)
         self.t += 1
--- Beta.py
+++ Beta.py
@@ -3,11 +3,10 @@ from scipy.special import btdtri
 
 class Beta:
     """
-    Computes the posterior of Bernoulli/Beta distribution.
+    This class computes the posterior of Bernoulli (/Beta) distribution.
 
     Parameters :
-    a,b
-        Shape parameters of the Beta distribution 
+    a,b : int. Shape parameters of the Beta distribution 
     """
 
     def __init__(self, a=1, b=1):
@@ -15,6 +14,11 @@ class Beta:
         self.b = b
 
     def reset(self, a=0, b=0):
+        '''
+        Reset the posterior to a "default" configuration.
+        Parameters:
+        a,b : int. Shape parameters of the Beta distribution.
+        '''
         if a == 0:
             a = self.a
         if b == 0:
@@ -22,10 +26,23 @@ class Beta:
         self.N = [a, b]
 
     def update(self, obs):
+        '''
+        Update the parameters of the distribution.
+        Parameters:
+        obs : float. Index of the parameter to be updated.
+        '''
         self.N[int(obs)] += 1
 
     def sample(self):
+        '''
+        Returns the parameters of the distribution
+        '''
         return betavariate(self.N[1], self.N[0])
 
     def quantile(self, p):
+        '''
+        Returns the p-th quantile of the beta distribution.
+        Parameters :
+        p : float. Cumulative probability in [0,1]
+        '''
         return btdtri(self.N[1], self.N[0], p)
