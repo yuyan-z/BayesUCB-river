c6381d18a8cb42d2d1d6f4acc74559754b0ba81d
jaulmes <bjaulmes@yahoo.fr>
2023-04-29 15:54:29 +0200

    rearranged classes

--- BayesUCB.py
+++ BayesUCB.py
@@ -2,35 +2,8 @@ import gym
 from river import bandit, proba
 from river import stats
 
-from random import betavariate
-from scipy.special import btdtri
 import random
-
-class Beta:
-    """
-    posterior of Bernoulli/Beta distribution.
-    """
-
-    def __init__(self, a=1, b=1):
-        self.a = a
-        self.b = b
-
-    def reset(self, a=0, b=0):
-        if a == 0:
-            a = self.a
-        if b == 0:
-            b = self.b
-        self.N = [a, b]
-
-    def update(self, obs):
-        self.N[int(obs)] += 1
-
-    def sample(self):
-        return betavariate(self.N[1], self.N[0])
-
-    def quantile(self, p):
-        return btdtri(self.N[1], self.N[0], p)
-
+from Beta import Beta
 
 class BayesUCB(bandit.base.Policy):
     def __init__(self, n_arms: int, reward_obj=None, burn_in=0):
@@ -59,23 +32,3 @@ class BayesUCB(bandit.base.Policy):
     def getReward(self, arm, reward):
         self.posterior[arm].update(reward)
         self.t += 1
-
-
-if __name__ == '__main__':
-    env = gym.make('river_bandits/CandyCaneContest-v0')
-    _ = env.reset(seed=42)
-    _ = env.action_space.seed(123)
-
-    policy = BayesUCB(n_arms=env.action_space.n)
-
-    metric = stats.Sum()
-    while True:
-        action = next(policy.pull(range(env.action_space.n)))
-        observation, reward, terminated, truncated, info = env.step(action)
-        policy = policy.update(action, reward)
-        policy.getReward(action, reward)
-        metric = metric.update(reward)
-        if terminated or truncated:
-            break
-
-    print('Sum:', metric.get())
--- Beta.py
+++ Beta.py
@@ -5,20 +5,21 @@ class Beta:
     """
     posterior of Bernoulli/Beta distribution.
     """
+
     def __init__(self, a=1, b=1):
         self.a = a
         self.b = b
-        
+
     def reset(self, a=0, b=0):
-        if a==0:
+        if a == 0:
             a = self.a
-        if b==0:
+        if b == 0:
             b = self.b
         self.N = [a, b]
 
     def update(self, obs):
         self.N[int(obs)] += 1
-        
+
     def sample(self):
         return betavariate(self.N[1], self.N[0])
 
