ed4bcdb5ff965cb8ede116e4ab9f6780c0a4b205
Yuyan Z <64955334+yuyan2000@users.noreply.github.com>
2023-06-07 20:29:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import gym
-import copy
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -13,42 +12,6 @@ from bayes_ucb import BayesUCB
 def pull_func(policy, env):
     return next(policy.pull(range(env.action_space.n)))
 
-
-def test(policies, env, n_episodes, metrictype):
-    trace = []
-
-    for policy_idx, policy in enumerate(policies):
-        for episode in range(n_episodes):
-            episode_policy = policy.clone()
-            episode_env = copy.deepcopy(env)
-            episode_env.reset(seed=42)
-            episode_env.action_space.seed(123)
-            step = 0
-
-            metric = stats.Sum()
-            if metrictype == 'mean reward':
-                metric = stats.Mean()
-
-            while True:
-                action = next(episode_policy.pull(range(env.action_space.n)))
-                observation, reward, terminated, truncated, info = episode_env.step(action)
-                metric = metric.update(reward)
-                episode_policy.update(action, reward)
-
-                trace.append({
-                    "episode": episode,
-                    "policy_idx": policy_idx,
-                    "step": step,
-                    "reward": reward,
-                    metrictype: metric.get(),
-                })
-                step += 1
-
-                if terminated or truncated:
-                    break
-
-    trace_df = pd.DataFrame(trace)
-    return trace_df
 
 if __name__ == '__main__':
     policies = [
